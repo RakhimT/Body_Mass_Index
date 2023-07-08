e18d4179a6d301342085698f9ff359ed20366216
Rakhim <rakhimturaliev@gmail.com>
2023-07-08 22:25:13 +0300

    Initial commit

---
+++ src/main/java/BodyMassIndexService/BodyMassIndexService.java
@@ -0,0 +1,10 @@
+package BmiService;
+public class BodyMassIndexService {
+    public int calculate(int bMiS) {
+        double weight = 92.5;
+        double height = 1.85;
+        return weight / height;
+    }
+
+
+}
---
+++ src/main/java/com/example/body_mass_index/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.body_mass_index;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
